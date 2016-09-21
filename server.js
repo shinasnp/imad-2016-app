5c2f22d058332dc0dc8e712eb11581bfa2d1b18a
shinas.np <shinasnp@users.noreply.github.com>
2016-09-21 11:29:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile("This is new article one");
+  res.send('This is new article one');
 });
 
 app.get('/ui/style.css', function (req, res) {
