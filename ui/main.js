6e2cde18809eea13e3990f791ff569e7fa4068b1
shinas.np <shinasnp@users.noreply.github.com>
2016-10-04 15:19:26 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,83 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
+var element = document.getElementById("main-text");
+element.innerHTML = "New Value";
+
+var img = document.getElementById("madi");
+var marginLeft = 0;
+
+
+function moveRight () {
+	marginLeft = marginLeft + 5;
+	img.style.marginLeft = marginLeft + 'px';
+};
+
+img.onclick = function(){
+	var intervel = setInterval(moveRight,5)
+};
+
+*/
+// counter add ajax
+var button = document.getElementById("counter");
+button.onclick = function(){
+	var request = new XMLHttpRequest();
+
+
+
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+
+
+			if (request.status === 200 ){
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	}
+	request.open('GET','http://localhost:8080/counter',true);
+	request.send(null);
+};
+
+//submit name
+
+var submit = document.getElementById('submit_btn')
+submit.onclick = function(){
+	var request = new XMLHttpRequest();
+
+
+
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+
+
+			if (request.status === 200 ){
+				var names = request.responseText;
+				names = JSON.parse(names);
+				var list = '';
+				for(var i=0; i<names.length; i++){
+
+					list += '<li>' + names[i] + '</li>';
+				}
+				var ul = document.getElementById('namelist');
+				ul.innerHTML = list;
+				
+				 
+			}
+		}
+
+	
+	
+	
+};
+	var nameInput = document.getElementById('name');
+	var name = nameInput.value;
+	request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+	request.send(null);
+	};
+
+	
+
+
+
+
+
