3a3ac608be434dc31ea0b81d9bd7553aa9e4a9c8
shinas.np <shinasnp@users.noreply.github.com>
2016-10-04 15:18:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -81,13 +81,30 @@ function createTemplate(data){
 
 
 
+
+
+
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter +1;
+  res.send(counter.toString());
+});
+var names = [];
+app.get('/submit-name', function (req, res) {
+	var name = req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
 app.get('/:articleName', function (req, res) {
 	var articleName = req.params.articleName;
-	console.log("aaaaa",articleName)
+	
   res.send(createTemplate(articles[articleName]));
 });
 
@@ -96,15 +113,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/mario.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'mario.png'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`Imad  on port ${port}!`);
 });
